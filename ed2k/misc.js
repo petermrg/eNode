f250a618404bc018fc68901d1d62c2c110261783
petermrg <petermrg@ymail.com>
2013-03-11 21:01:32 +0100

    little clean up

--- ed2k/misc.js
+++ ed2k/misc.js
@@ -1,5 +1,7 @@
 
-exports.ext = function(name) { return name.substring(name.lastIndexOf('.')+1, name.length).toLowerCase(); };
+exports.ext = function(name) {
+    return name.substring(name.lastIndexOf('.')+1, name.length).toLowerCase();
+};
 
 exports.hex = function(n, len) {
     n = (n*1).toString(16);
@@ -12,14 +14,24 @@ exports.IPv4toInt32LE = function(IPv4) {
     ip = (ip[0]*1) + (ip[1]*0x100) + (ip[2]*0x10000) + (ip[3]*0x1000000);
     if (ip<0) log.error(ip);
     return ip;
-}
+};
 
 var extensions = {
-    video: '3gp,aaf,asf,avchd,avi,fla,flv,m1v,m2v,m4v,mp4,mpg,mpe,mpeg,mov,mkv,mp4,ogg,rm,svi'.split(','),
-    audio: 'aiff,au,wav,flac,la,pac,m4a,ape,rka,shn,tta,wv,wma,brstm,amr,mp2,mp3,ogg,aac,m4a,mpc,ra,ots,vox,voc,mid,mod,s3m,xm,it,asf'.split(','),
-    image: 'cr2,pdn,pgm,pict,bmp,png,dib,djvu,gif,psd,pdd,icns,ico,rle,tga,jpeg,jpg,tiff,tif,jp2,jps,mng,xbm,xcf,pcx'.split(','),
-    pro  : '7z,ace,arc,arj,bzip2,cab,gzip,rar,tar,zip,iso,nrg,img,adf,dmg,cue,bin,cif,ccd,sub,raw'.split(','),
-}
+    video:
+        '3gp,aaf,asf,avchd,avi,fla,flv,m1v,m2v,m4v,mp4,mpg,mpe,mpeg,mov,mkv,mp4,ogg,rm,svi'
+        .split(','),
+    audio:
+        'aiff,au,wav,flac,la,pac,m4a,ape,rka,shn,tta,wv,wma,brstm,amr,mp2,mp3,ogg,aac,m4a,mpc,ra,'+
+        'ots,vox,voc,mid,mod,s3m,xm,it,asf'
+        .split(','),
+    image:
+        'cr2,pdn,pgm,pict,bmp,png,dib,djvu,gif,psd,pdd,icns,ico,rle,tga,jpeg,jpg,tiff,tif,jp2,'+
+        'jps,mng,xbm,xcf,pcx'
+        .split(','),
+    pro:
+        '7z,ace,arc,arj,bzip2,cab,gzip,rar,tar,zip,iso,nrg,img,adf,dmg,cue,bin,cif,ccd,sub,raw'+
+        .split(','),
+};
 
 exports.getFileType = function(name) {
     if (!typeof name == 'string') { return '' };
