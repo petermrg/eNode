b11956a63534ef158528584eb307dc598337256b
petermrg <petermrg@ymail.com>
2013-09-21 13:25:20 +0200

    cleanup

--- enode/storage/storage.js
+++
@@ -1,7 +0,0 @@
-var conf = require('../enode.config.js').config
-var engine = require('./engine.'+conf.storage.engine+'.js')
-
-exports.init = engine.init
-exports.clients = engine.clients
-exports.files = engine.files
-exports.servers = engine.servers
